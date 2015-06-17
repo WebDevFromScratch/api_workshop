59de0ae220facd70a5336b20257157e25fabfa25
Piotr Klosinski <mybettingstuff@gmail.com>
2015-06-17 18:40:26 +0200

    set tests to pending

--- spec/api_workshop_spec.rb
+++ spec/api_workshop_spec.rb
@@ -16,7 +16,7 @@ describe ApiWorkshop do
 
     describe '/stories' do
       describe 'GET /' do
-        it 'returns 200 status response and a list of stories' do
+        xit 'returns 200 status response and a list of stories' do
           get '/api/stories'
 
           expect(last_response.status).to eq(200)
@@ -26,7 +26,7 @@ describe ApiWorkshop do
 
       describe 'GET /:id' do
         context 'a story exists' do
-          it 'returns 200 status response and a story details' do
+          xit 'returns 200 status response and a story details' do
             get "/api/stories/#{@story1.id}"
 
             expect(last_response.status).to eq(200)
@@ -36,7 +36,7 @@ describe ApiWorkshop do
         end
 
         context 'a story doesn\'t exist' do
-          it 'returns a 404 status response and an expected error' do
+          xit 'returns a 404 status response and an expected error' do
             get '/api/stories/12223421231'
 
             expect(last_response.status).to eq(404)
@@ -47,7 +47,7 @@ describe ApiWorkshop do
 
       describe 'POST /' do
         context 'with valid data' do
-          it 'returns 201 status response and a story details' do
+          xit 'returns 201 status response and a story details' do
             post '/api/stories', {url: 'story url', title: 'story title'}.to_json, 'CONTENT_TYPE' => 'application/json'
 
             expect(last_response.status).to eq(201)
@@ -57,7 +57,7 @@ describe ApiWorkshop do
         end
 
         context 'with invalid data' do
-          it 'returns 422 status and an expected error' do
+          xit 'returns 422 status and an expected error' do
             post '/api/stories', {url: '', title: ''}.to_json, 'CONTENT_TYPE' => 'application/json'
 
             expect(last_response.status).to eq(422)
@@ -66,7 +66,7 @@ describe ApiWorkshop do
         end
 
         context 'when a story url already exists in the database' do
-          it 'returns 409 status and an expected error' do
+          xit 'returns 409 status and an expected error' do
             post '/api/stories', {url: 'http://story1.com', title: 'story title'}.to_json, 'CONTENT_TYPE' => 'application/json'
 
             expect(last_response.status).to eq(409)
@@ -77,7 +77,7 @@ describe ApiWorkshop do
 
       describe 'PATCH /:id' do
         context 'with valid data' do
-          it 'returns 204 status' do
+          xit 'returns 204 status' do
             patch "/api/stories/#{@story1.id}", {body: 'new story body'}.to_json, 'CONTENT_TYPE' => 'application/json'
 
             expect(last_response.status).to eq(204)
@@ -85,7 +85,7 @@ describe ApiWorkshop do
         end
 
         context 'with invalid data' do
-          it 'returns 422 status and an expected error' do
+          xit 'returns 422 status and an expected error' do
             patch "/api/stories/#{@story1.id}", {body: ''}.to_json, 'CONTENT_TYPE' => 'application/json'
 
             expect(last_response.status).to eq(422)
@@ -96,7 +96,7 @@ describe ApiWorkshop do
 
       describe 'POST /:id/vote/up' do
         context 'when a user has not upvote yet' do
-          it 'returns 200 status' do
+          xit 'returns 200 status' do
             post "/api/stories/#{@story1.id}/vote/up"
 
             expect(last_response.status).to eq(200)
@@ -108,7 +108,7 @@ describe ApiWorkshop do
         context 'when a user has already casted an upvote' do
           before { post "/api/stories/#{@story1.id}/vote/up" }
 
-          it 'returns 409 status and an expected body' do
+          xit 'returns 409 status and an expected body' do
             post "/api/stories/#{@story1.id}/vote/up"
 
             expect(last_response.status).to eq(409)
@@ -119,7 +119,7 @@ describe ApiWorkshop do
 
       describe 'POST /:id/vote/down' do
         context 'when a user has not upvote yet' do
-          it 'returns 200 status' do
+          xit 'returns 200 status' do
             post "/api/stories/#{@story1.id}/vote/down"
 
             expect(last_response.status).to eq(200)
@@ -130,7 +130,7 @@ describe ApiWorkshop do
         context 'when a user has already casted an upvote' do
           before { post "/api/stories/#{@story1.id}/vote/down" }
 
-          it 'returns 409 status and an expected body' do
+          xit 'returns 409 status and an expected body' do
             post "/api/stories/#{@story1.id}/vote/down"
 
             expect(last_response.status).to eq(409)
@@ -143,7 +143,7 @@ describe ApiWorkshop do
         context 'when a user already casted a vote' do
           before { post "/api/stories/#{@story1.id}/vote/up" }
 
-          it 'returns 204 status' do
+          xit 'returns 204 status' do
             delete "/api/stories/#{@story1.id}/vote"
 
             expect(last_response.status).to eq(204)
@@ -151,7 +151,7 @@ describe ApiWorkshop do
         end
 
         context 'when a user did not cast a vote yet' do
-          it 'returns 422 status and an expected error' do
+          xit 'returns 422 status and an expected error' do
             delete "/api/stories/#{@story1.id}/vote"
 
             expect(last_response.status).to eq(422)
@@ -164,7 +164,7 @@ describe ApiWorkshop do
     describe '/users' do
       describe 'POST /' do
         context 'with valid data' do
-          it 'returns 201 status' do
+          xit 'returns 201 status' do
             post '/api/users', {username: 'JohnDoe', password: 'secret123'}.to_json, 'CONTENT_TYPE' => 'application/json'
 
             expect(last_response.status).to eq(201)
@@ -174,7 +174,7 @@ describe ApiWorkshop do
         end
 
         context 'with invalid data' do
-          it 'returns 422 status and an expected error' do
+          xit 'returns 422 status and an expected error' do
             post '/api/users', {username: '', password: ''}.to_json, 'CONTENT_TYPE' => 'application/json'
 
             expect(last_response.status).to eq(422)
@@ -185,7 +185,7 @@ describe ApiWorkshop do
         context 'when a username is already taken' do
           before { User.create(username: 'JohnDoe', password: 'secret123') }
 
-          it 'returns 409 status and an expected error' do
+          xit 'returns 409 status and an expected error' do
             post '/api/users', {username: 'JohnDoe', password: 'secret567'}.to_json, 'CONTENT_TYPE' => 'application/json'
 
             expect(last_response.status).to eq(409)
