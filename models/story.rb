9d7a2c3b2accb08d43d82a165010b6cd6b4a2937
Piotr Klosinski <mybettingstuff@gmail.com>
2015-06-19 16:18:54 +0200

    [#4] add Vote, create associations

--- models/story.rb
+++ models/story.rb
@@ -1,4 +1,6 @@
 class Story < ActiveRecord::Base
   validates :url, presence: true, uniqueness: true
   validates :title, presence: true
+
+  has_many :votes
 end
--- models/user.rb
+++ models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
+
+  has_many :votes
 end
