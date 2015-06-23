19245795bad51e2f065e9d45171d9d708a8f6ff9
Piotr Klosinski <mybettingstuff@gmail.com>
2015-06-23 10:37:07 +0200

    add focus filter to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ Dir["./controllers/*.rb"].each {|file| require file }
 
 RSpec.configure do |config|
   config.include RequestHelpers
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
