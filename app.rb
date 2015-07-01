dce63c28082adbfce02006f0816180f7c06d5ea9
Piotr Klosinski <mybettingstuff@gmail.com>
2015-07-01 08:38:53 +0200

    [#8] configure Rack::Cache with dalli

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/router'
+require 'dalli'
+require 'rack-cache'
 require './config/environment'
 Dir.glob('./{models,controllers}/*.rb').each { |file| require file }
 Dir.glob('./v*/{models,controllers}/*.rb').each { |file| require file }
